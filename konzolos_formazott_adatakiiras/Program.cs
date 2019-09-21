d1fefb16cf19ceb484f7365c38a5c0c3ddfb5951
Beregszászi Pál Szakközépiskola <bpszki@outlook.hu>
2019-09-21 19:35:46 +0200

    Add project files.

---
+++ konzolos_formazott_adatakiiras/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace konzolos_formazott_adatakiiras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
